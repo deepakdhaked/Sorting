3126abf217c35b7033e1c236a7d703e00b699168
deepak dhaked <deepakdhakedpc@gmail.com>
2023-07-04 15:08:48 +0530

    insertion sort is added

---
+++ insertion_sort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+// A utility function to print an array of size n
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Worst Case Time Complexity : O(N^2)
+// Average Case Time Complexity : O(N^2)
+// Best Case Time Complexity : O(N)
+// Auxiliary Space: O(1)
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
+    printArray(arr, n);
+
+}
